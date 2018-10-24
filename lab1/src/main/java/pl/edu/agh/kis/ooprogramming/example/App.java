b2e43d6e5aba1923db55605ba4b209ce7ef931e4
Michał Wypych <micwypych@gmail.com>
2018-10-24 18:45:05 +0200

    Replaces System.out.println with log.info message; Look at the error message by PMD!

--- lab1/src/main/java/pl/edu/agh/kis/ooprogramming/example/App.java
+++ lab1/src/main/java/pl/edu/agh/kis/ooprogramming/example/App.java
@@ -1,11 +1,17 @@
 package pl.edu.agh.kis.ooprogramming.example;
 
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class App {
     public static void main(String[] args) {
         App application = new App();
-        System.out.println("Value retruned from library is: " + application.library.someLibraryMethod());
+        log.info("Value retruned from library is: {}", application.library.someLibraryMethod());
     }
+
+    private static final Logger log =  LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private Library library = new Library();
 } 
